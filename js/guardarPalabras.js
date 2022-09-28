1fb0259f8cc59935df1db2865611a06e5f6da313
Carlos-Alvarado <alvaradocarlosluis97@gmail.com>
2022-09-28 10:55:59 -0300

    Agregando mensajes de juego ganado/perdido

--- js/guardarPalabras.js
+++ js/guardarPalabras.js
@@ -1,14 +1,19 @@
 let botonGuardar = document.querySelector('.boton-guardar')
-let i = (localStorage.length==0)?0:localStorage.length;
-botonGuardar.addEventListener('click',e=>{
+let i = (localStorage.length == 0) ? 0 : localStorage.length;
+botonGuardar.addEventListener('click', e => {
     e.preventDefault()
     let agregarPalabra = document.querySelector('.agregar-palabra').value
-    if(agregarPalabra.length<=8){
+    if (agregarPalabra.length <= 8 && agregarPalabra.length>=3) {
         console.log(agregarPalabra);
-        localStorage.setItem(i,agregarPalabra)
+        localStorage.setItem(i, agregarPalabra)
+        window.location.href = '../pages/juego.html'
     }
-    else{
-        console.log('Palabra no valida');
+    else {
+        Swal.fire(
+            'Palabra no valida',
+            'Ingresa una palabra que tenga como maximo 8 caracteres',
+            'error'
+        )
     }
-    window.location.href='../pages/juego.html'
+    
 })
--- js/juego.js
+++ js/juego.js
@@ -1,10 +1,9 @@
-let palabras = ['javascript', 'mundo', 'programacion', 'codigo', 'computadora'];
 
-for (let i = 0; i < localStorage.length; i++) {
-    palabras.push(localStorage.getItem(i))
-    
+function cargarPalabras() {
+    for (let i = 0; i < localStorage.length; i++) {
+        palabras.push(localStorage.getItem(i))
+    }
 }
-
 function graficar([xm, ym, xp, yp], intento) {
     if (intento !== 4) {
         ctx.moveTo(xm, ym)
@@ -18,83 +17,99 @@ function graficar([xm, ym, xp, yp], intento) {
 
 
 }
-const intentos = 8
+function colocarGuiones() {
+    let guiones = document.querySelector('.guiones')
+    let letras = document.querySelector('.letras')
+    for (let i = 0; i < palabra.length; i++) {
+        let div = document.createElement('div')
+        let divLetra = document.createElement('div')
+        divLetra.textContent = " "
+        div.textContent = ' '
+        div.classList.add("guion")
+
+        divLetra.classList.add('letraJuego')
+        letras.appendChild(divLetra)
+        guiones.appendChild(div)
+    }
+}
+
+let palabras = ['javascript', 'mundo', 'programacion', 'codigo', 'computadora'];
 let intento = 0;
-//const palabras = ['javascript', 'mundo', 'programacion', 'codigo', 'computadora']
-
-
-
-
 let juegoAhorcado = document.querySelector('#juego');
-
+const intentos = 8
+const movimietosPincel = [[150, 400, 350, 400], [200, 400, 200, 100], [200, 100, 300, 100], [300, 100, 300, 170], [300, 200, 30, 0], [300, 230, 300, 300], [300, 300, 270, 340], [300, 300, 330, 340], [300, 250, 270, 280], [300, 250, 330, 280]]
 const canvas = document.getElementById('tutorial');
 const ctx = canvas.getContext('2d');
+let palabra = palabras[Math.floor(Math.random() * (palabras.length))]
+//Configuracion del canvas
 ctx.lineWidth = 5
 ctx.fillStyle = "rgb(200,0,0)";
-
 ctx.lineCap = 'round';
 
-//base
-let movimietosPincel = [[150, 400, 350, 400], [200, 400, 200, 100], [200, 100, 300, 100], [300, 100, 300, 170], [300, 200, 30, 0], [300, 230, 300, 300], [300, 300, 270, 340], [300, 300, 330, 340], [300, 250, 270, 280], [300, 250, 330, 280]]
+function main() {
 
-let palabra = palabras[Math.floor(Math.random() * (palabras.length))]
-console.log(palabra);
-// console.log(Math.random()*(palabras.length-1);
-let guiones = document.querySelector('.guiones')
-let letras = document.querySelector('.letras')
-for (let i = 0; i < palabra.length; i++) {
-    let div = document.createElement('div')
-    let divLetra = document.createElement('div')
-    divLetra.textContent = " "
-    div.textContent = ' '
-    div.classList.add("guion")
 
-    divLetra.classList.add('letraJuego')
-    letras.appendChild(divLetra)
-    guiones.appendChild(div)
-}
-let letrasIngresadas = []
-document.addEventListener('keydown', e => {
-    let letrasJuego = document.querySelectorAll('.guion')
-    console.dir(letrasJuego);
-    console.log(letrasIngresadas);
-    let letraIngresada = e.key.toUpperCase()
-    console.log(!letrasIngresadas.includes(letraIngresada));
-    if (!letrasIngresadas.includes(letraIngresada)) {
-        if ((palabra.toUpperCase().includes(letraIngresada))) {
-            for (let i = 0; i < palabra.length; i++) {
-                if (e.key.toUpperCase() === palabra[i].toUpperCase()) {
-                    console.log(palabra[i]);
-                    letrasJuego[i].textContent = palabra[i].toUpperCase()
+    cargarPalabras();
+    colocarGuiones();
+
+
+    let letrasIngresadas = []
+    document.addEventListener('keydown', e => {
+        let letrasJuego = document.querySelectorAll('.guion')
+        let letraIngresada = e.key.toUpperCase()
+        if (!letrasIngresadas.includes(letraIngresada)) {
+            if ((palabra.toUpperCase().includes(letraIngresada))) {
+                for (let i = 0; i < palabra.length; i++) {
+                    if (e.key.toUpperCase() === palabra[i].toUpperCase()) {
+                        letrasJuego[i].textContent = palabra[i].toUpperCase()
+                    }
                 }
-            }
-            let bandera = true
-            for (let i = 0; i < letrasJuego.length; i++) {
-                if (letrasJuego[i].textContent == ' ') {
-                    bandera = false
-                    break
+                let bandera = true
+                for (let i = 0; i < letrasJuego.length; i++) {
+                    if (letrasJuego[i].textContent == ' ') {
+                        bandera = false
+                        break
+                    }
                 }
-            }
-            if (bandera) {
-                let mensaje = document.querySelector('.mensaje')
-                mensaje.textContent = 'Felicidades , Ganaste!'
-            }
+                if (bandera) {
+                   
+                    Swal.fire({
+                        title: 'Felicidades ,Ganaste!',
+                        showClass: {
+                            popup: 'animate__animated animate__fadeInDown',
+                        },
+                        hideClass: {
+                            popup: 'animate__animated animate__fadeOutUp'
+                        }
+                    }).then(function() {
+                        window.location = "../pages/juego.html";
+                    });
+                }
 
 
-        } else {
-            graficar(movimietosPincel[intento], intento);
-            intento++;
-            if (intento > intentos) {
-                let mensaje = document.querySelector('.mensaje')
-                mensaje.textContent = 'Fin del Juego'
-            }
+            } else {
+                graficar(movimietosPincel[intento], intento);
+                intento++;
+                if (intento > intentos) {
+                    Swal.fire({
+                        title: 'Fin del juego',
+                        showClass: {
+                            popup: 'animate__animated animate__fadeInDown',
+                        },
+                        hideClass: {
+                            popup: 'animate__animated animate__fadeOutUp'
+                        }
+                    }).then(function() {
+                        window.location = "../pages/juego.html";
+                    });
+                }
 
+            }
+            letrasIngresadas.push(e.key.toUpperCase())
+            let letrasIngresadasPorTeclado = document.querySelector('.letras-ingresadas')
+            letrasIngresadasPorTeclado.textContent = letrasIngresadasPorTeclado.textContent + e.key.toUpperCase()
         }
-        letrasIngresadas.push(e.key.toUpperCase())
-        let letrasIngresadasPorTeclado =  document.querySelector('.letras-ingresadas')
-        letrasIngresadasPorTeclado.textContent = letrasIngresadasPorTeclado.textContent + e.key.toUpperCase()
-    }
+    })
+}
 
-
-    console.log(e);
-})
+main();
